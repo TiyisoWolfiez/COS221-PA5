a65b5eafac343700156d3b0270162258f4cd1da5
Michael <93211335+waveyboym@users.noreply.github.com>
2023-05-20 16:34:20 +0200

    Add files via upload

---
+++ project/FrontEnd/Client/SignupLogin.js
@@ -0,0 +1,143 @@
+let currentSelectionLogin = true;
+/*
+RegEx	Description
+^	The password string will start this way
+(?=.*[a-z])	The string must contain at least 1 lowercase alphabetical character
+(?=.*[A-Z])	The string must contain at least 1 uppercase alphabetical character
+(?=.*[0-9])	The string must contain at least 1 numeric character
+(?=.*[!@#$%^&*])	The string must contain at least one special character, but we are escaping reserved RegEx characters to avoid conflict
+(?=.{8,})	The string must be eight characters or longer
+*/
+const passWordReg = new RegExp("^((?=.{8,})(?=.*[A-Z])(?=.*[a-z])(?=.*[0-9])(?=.*[!@#$%^&*?><.,;:]))");
+const regexEmail = /^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/;
+const nameAndSurnameReg = new RegExp("^((?=.{2,})(?=.*[a-z]))");
+
+const toggleSignUpLogin = function(){
+    if(currentSelectionLogin){
+        document.querySelector(".signup-login-box").innerHTML = '<form action="#" method="post" onsubmit="return validateSignUp()" class="needs-validation">' +
+            '<div class="row align-items-center rounded-right-3">' +
+                '<div class="header-text mb-1">' +
+                    '<h3>Welcome to Winery SA</h3>' +
+                    '<p>Sign up to access the finest wineries</p>' +
+                '</div>' +
+                '<div class="input-group mb-2 was-validated">' +
+                    '<input type="text"  id="name" class="form-control form-control-lg bg-light fs-6" placeholder="first name" required/>' +
+                '</div>' +
+                '<div class="input-group mb-2 was-validated">' +
+                    '<input type="text"  id="surname" class="form-control form-control-lg bg-light fs-6" placeholder="surname" required/>' +
+                '</div>' +
+                '<div class="input-group mb-2 was-validated">' +
+                    '<input type="email"  id="email" class="form-control form-control-lg bg-light fs-6" placeholder="email address" required/>' +
+                '</div>' +
+                '<div class="input-group mb-2 was-validated">' +
+                    '<input type="password"  id="password" class="form-control form-control-lg bg-light fs-6" placeholder="password" required/>' +
+                '</div>' +
+                '<div class="input-group mb-2">' +
+                    '<input type="submit" class="btn btn-lg w-100 fs-6 login-btn" value="Signup">' +
+                '</div>' +
+                '<div class="input-group mb-1">' +
+                    '<button class="btn btn-lg btn-light w-100 fs-6 no-account-btn" onclick="toggleSignUpLogin()">Have an account? Login instead</button>' +
+                '</div>'+
+                '<p class="text-danger error-container"></p>'+
+            '</div>'+
+        '</form>';
+        currentSelectionLogin = false;
+    }
+    else{
+        document.querySelector(".signup-login-box").innerHTML = '<form action="#" method="post" onsubmit="return validateLogin()" class="needs-validation">' +
+            '<div class="row align-items-center rounded-right-3">' +
+                '<div class="header-text mb-4">' +
+                    '<h3>Welcome back to Winery SA</h3>' +
+                    '<p>We are glad to see you again</p>' +
+                '</div>' +
+                '<div class="input-group mb-3 was-validated">' +
+                    '<input type="email" id="email" class="form-control form-control-lg bg-light fs-6" placeholder="email address" required/>' +
+                '</div>' +
+                '<div class="input-group mb-3 was-validated">' +
+                    '<input type="password"  id="password" class="form-control form-control-lg bg-light fs-6" placeholder="password" required/>' +
+                '</div>' +
+                '<div class="input-group mb-3">' +
+                    '<input type="submit" class="btn btn-lg w-100 fs-6 login-btn" value="Login">' +
+                '</div>' +
+                '<div class="input-group mb-3">' +
+                    '<button class="btn btn-lg btn-light w-100 fs-6 no-account-btn" onclick="toggleSignUpLogin()">Don\'t have an account? Signup instead</button>' +
+                '</div>'+
+                '<p class="text-danger error-container"></p>'+
+            '</div>'+
+        '</form>';
+        currentSelectionLogin = true;
+    }
+};
+
+
+
+const validateSignUp = function(){
+    const getName = document.getElementById("name").value;
+    const getSurname = document.getElementById("surname").value;
+    const getEmail = document.getElementById("email").value;
+    const getPassword = document.getElementById("password").value;
+
+    if(getName == "" || getSurname == "" || getEmail == "" || getPassword == ""){
+        document.querySelector(".error-container").innerHTML = "please fill in the form";
+        return false;
+    }
+
+    if(nameAndSurnameReg.test(getName)){//name is valid
+        if(nameAndSurnameReg.test(getSurname)){//surname is valid
+            if(getEmail.match(regexEmail)){//email valid
+                if(passWordReg.test(getPassword)){//password valid
+                    return true;
+                }
+                else{
+                    document.querySelector(".error-container").innerHTML = "Password should be greater than 8 characters and have upper and lowercase letters with symbols( eg: %#&)";
+                    document.getElementById("password").value = "";
+                }
+            }
+            else{
+                document.querySelector(".error-container").innerHTML = "please fill in a valid email";
+                document.getElementById("email").value = "";
+                document.getElementById("password").value = "";
+            }
+        }
+        else{
+            document.querySelector(".error-container").innerHTML = "please fill in a valid surname";
+            document.getElementById("surname").value = "";
+            document.getElementById("password").value = "";
+        }
+    }
+    else{
+        document.querySelector(".error-container").innerHTML = "please fill in a valid name";
+        document.getElementById("name").value = "";
+        document.getElementById("password").value = "";
+    }
+    return false;
+};
+
+
+const validateLogin = function(){
+    const getEmail = document.getElementById("email").value;
+    const getPassword = document.getElementById("password").value;
+
+    if(getEmail == "" || getPassword == ""){
+        document.querySelector(".error-container").innerHTML = "please fill in the form";
+        return false;
+    }
+
+    
+    if(getEmail.match(regexEmail)){//email valid
+        if(passWordReg.test(getPassword)){//password valid
+            return true;
+        }
+        else{
+            document.querySelector(".error-container").innerHTML = "Password should be greater than 8 characters and have upper and lowercase letters with symbols( eg: %#&)";
+            document.getElementById("password").value = "";
+        }
+    }
+    else{
+        document.querySelector(".error-container").innerHTML = "please fill in a valid email";
+        document.getElementById("email").value = "";
+        document.getElementById("password").value = "";
+    }
+    return false;
+
+};
---
+++ project/ReadMe.txt
@@ -0,0 +1,12 @@
+//////////////////Navigating this project is as follows/////////////////////
+
+1. Backend contains all the php code that connects and talks to the database
+    i) Config contains the logic for connecting to the database
+    ii) login contains the logic for logging whether sign-up or sign-in in a user to the website
+    iii) logout log outs a user from the site
+    iv) update updates the backend with data from the frontend asynchronously
+2. Frontend contains all the pages that the user views when they visit the website
+    i) Assets contains all the assets a user see's when they visit the page, eg images, svg, videos etc.
+    ii) Client contains all the client code that talks to the backend
+    iii) Pages contains the actual pages a user sees when they visit the website
+    iv) Styles contains the stylings for the web pages
