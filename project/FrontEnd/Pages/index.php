f2a8b4e961424691292a1f4338d0b45cacba8e08
Michael <u21546551@tuks.co.za>
2023-05-28 13:58:51 +0200

    these are just test files, please don't merge

---
+++ project/FrontEnd/Pages/index.php
@@ -0,0 +1,95 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-KK94CHFLLe+nY2dmCWGMq91rCGa5gtU4mk92HdvYe+M/SXH301p5ILy+dN9+nJOZ" crossorigin="anonymous">
+    <link rel="stylesheet" href="../Styles/global.css">
+    <link rel="stylesheet" href="../Styles/home.css">
+    <script src="https://kit.fontawesome.com/d271141ba3.js" crossorigin="anonymous"></script>
+    <link rel="icon" type="image/svg+xml" href="../Assets/wine-glass-solid.svg" />
+    <link href="https://fonts.googleapis.com/css2?family=Great+Vibes&display=swap" rel="stylesheet">
+    <title>Winery SA | Home</title>
+</head>
+<body>
+    <?php include "../Components/Navbar.php";?>
+    <div class="landing-page-container">
+        <h1>Welcome to <br> Winery SA <i class="fa-solid fa-wine-glass pe-3" style="font-size: 5rem;"></i></h1>
+        <h3>Guiding you to the finest wines in South Africa</h3>
+
+        <h4>scroll down</h4>
+        <i class="fa-solid fa-chevron-down" onclick="document.getElementById('home-page-content').scrollIntoView({behavior: 'smooth'});"></i>
+    </div>
+
+    <div class="home-page-content" id="home-page-content">
+        <div class="card mb-3 card-info-container" style="max-width: 540px; height: 200px; max-height: 200px; overflow: hidden;">
+            <div class="row g-0">
+              <div class="col-md-4">
+                <img class="card-img-top " src="https://images.unsplash.com/photo-1504279577054-acfeccf8fc52?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=774&q=80" alt="...">
+              </div>
+              <div class="col-md-8">
+                <div class="card-body">
+                  <h5 class="card-title">Card title</h5>
+                  <p class="card-text">This is a wider card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.</p>
+                  <p class="card-text"><small class="text-muted">Last updated 3 mins ago</small></p>
+                </div>
+              </div>
+            </div>
+        </div>
+        <div class="card mb-3 card-info-container" style="max-width: 540px;">
+            <div class="row g-0">
+              <div class="col-md-4">
+                <img class="card-img-top" src="https://images.unsplash.com/photo-1547595628-c61a29f496f0?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=387&q=80" alt="...">
+              </div>
+              <div class="col-md-8">
+                <div class="card-body">
+                  <h5 class="card-title">Card title</h5>
+                  <p class="card-text">This is a wider card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.</p>
+                  <p class="card-text"><small class="text-muted">Last updated 3 mins ago</small></p>
+                </div>
+              </div>
+            </div>
+        </div>
+        <div class="card mb-3 card-info-container" style="max-width: 540px;">
+            <div class="row g-0">
+              <div class="col-md-4">
+                <img class="card-img-top" src="https://images.unsplash.com/photo-1578911373434-0cb395d2cbfb?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=387&q=80" alt="...">
+              </div>
+              <div class="col-md-8">
+                <div class="card-body">
+                  <h5 class="card-title">Card title</h5>
+                  <p class="card-text">This is a wider card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.</p>
+                  <p class="card-text"><small class="text-muted">Last updated 3 mins ago</small></p>
+                </div>
+              </div>
+            </div>
+        </div>
+        <div class="card mb-3 card-info-container" style="max-width: 540px;">
+            <div class="row g-0">
+              <div class="col-md-4">
+                <img class="card-img-top" src="https://images.unsplash.com/photo-1491924778227-f225b115dd5f?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=870&q=80" alt="...">
+              </div>
+              <div class="col-md-8">
+                <div class="card-body">
+                  <h5 class="card-title">Card title</h5>
+                  <p class="card-text">This is a wider card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.</p>
+                  <p class="card-text"><small class="text-muted">Last updated 3 mins ago</small></p>
+                </div>
+              </div>
+            </div>
+        </div>
+    </div>
+    <?php include "../Components/Footer.php";?>
+  <script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.11.7/dist/umd/popper.min.js" integrity="sha384-zYPOMqeu1DAVkHiLqWBUTcbYfZ8osu1Nd6Z89ify25QV9guujx43ITvfi12/QExE" crossorigin="anonymous"></script>
+  <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha3/dist/js/bootstrap.min.js" integrity="sha384-Y4oOpwW3duJdCWv5ly8SCFYWqFDsfob/3GkgExXKV4idmbt98QcxXYs9UoXAB7BZ" crossorigin="anonymous"></script>
+  <script>
+    const navEl = document.querySelector(".navbar-noscroll");
+
+    window.addEventListener("scroll", () => {
+        if(window.scrollY > 300)navEl.classList.add("navbar-scroll");
+        else navEl.classList.remove("navbar-scroll");
+    });
+  </script>
+</body>
+</html>
---
+++ project/FrontEnd/Pages/profile.php
@@ -0,0 +1,213 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-KK94CHFLLe+nY2dmCWGMq91rCGa5gtU4mk92HdvYe+M/SXH301p5ILy+dN9+nJOZ" crossorigin="anonymous">
+    <link rel="stylesheet" href="../Styles/global.css">
+    <link rel="stylesheet" href="../Styles/wineries.css">
+    <script src="https://kit.fontawesome.com/d271141ba3.js" crossorigin="anonymous"></script>
+    <link rel="icon" type="image/svg+xml" href="../Assets/wine-glass-solid.svg" />
+    <title>Winery SA | Wineries</title>
+</head>
+<body>
+    <?php include "../Components/Navbar.php";?>
+    <nav style="height: 70px;"></nav><!--buffer for navbar-->
+    <nav style="height: 60px;">
+      <div class="ms-auto align-items-center d-flex filter-tab" style="height: 60px;">
+        <div class="ms-3 btn btn-light btn-rounded rounded-4 border border-dark-subtle filter-buttons" data-bs-toggle="modal" data-bs-target="#exampleModal">
+          <i class="fa-solid fa-filter pe-2"></i>filters
+        </div>
+        <div class="ms-3 btn btn-light btn-rounded rounded-4 border border-dark-subtle filter-buttons">Cape Town</div>
+        <div class="ms-3 btn btn-light btn-rounded rounded-4 border border-dark-subtle filter-buttons">Port Elizabeth</div>
+        <div class="ms-3 btn btn-light btn-rounded rounded-4 border border-dark-subtle filter-buttons">Durban</div>
+        <div class="ms-3 btn btn-light btn-rounded rounded-4 border border-dark-subtle filter-buttons">Johannesburg</div>
+        <div class="ms-3 btn btn-light btn-rounded rounded-4 border border-dark-subtle filter-buttons">Pretoria</div>
+        <div class="ms-3 btn btn-light btn-rounded rounded-4 border border-dark-subtle filter-buttons">East London</div>
+        <div class="ms-3 btn btn-light btn-rounded rounded-4 border border-dark-subtle filter-buttons">Pietermaritzburg</div>
+        <div class="ms-3 btn btn-light btn-rounded rounded-4 border border-dark-subtle filter-buttons">Bloemfontein</div>
+      </div>
+    </nav>
+    <nav class="website-container d-flex justify-content-evenly flex-wrap align-items-center overflow-y-auto">
+      <div class="card card-item rounded-2" style="width: 18rem;">
+        <div class="img-container">
+          <img class="card-img-top" src="https://media.istockphoto.com/id/978754758/photo/vineyard-sunset.jpg?s=612x612&w=0&k=20&c=xvJZ_Ldp_n_Ev0LjaNY_vgrLvdB_dBrT8SdRfPn6zd0=" alt="Card image cap">
+        </div>
+        <div class="card-body">
+          <h5 class="card-title">Hatfield</h5>
+          <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+        </div>
+        <ul class="list-group list-group-flush">
+          <li class="list-group-item">Rating: 4.5★</li>
+          <li class="list-group-item">Open Days: Mon - Fri</li>
+          <li class="list-group-item">Open times: 8am - 4pm</li>
+        </ul>
+      </div>
+      <div class="card card-item rounded-2" style="width: 18rem;">
+        <div class="img-container">
+          <img class="card-img-top" src="https://res.akamaized.net/domain/image/upload/t_web/c_crop,h_393,w_627,x_150,y_53/v1631241636/118115-56_ama7b5.jpg" alt="Card image cap">
+        </div>
+        <div class="card-body">
+          <h5 class="card-title">Hatfield</h5>
+          <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+        </div>
+        <ul class="list-group list-group-flush">
+          <li class="list-group-item">Rating: 4.5★</li>
+          <li class="list-group-item">Open Days: Mon - Fri</li>
+          <li class="list-group-item">Open times: 8am - 4pm</li>
+        </ul>
+      </div>
+      <div class="card card-item rounded-2" style="width: 18rem;">
+        <div class="img-container">
+          <img class="card-img-top" src="https://parissiswinery.com/wp-content/uploads/2022/01/parissis_winery_skiathos_greece-1.jpg" alt="Card image cap">
+        </div>
+        <div class="card-body">
+          <h5 class="card-title">Hatfield</h5>
+          <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+        </div>
+        <ul class="list-group list-group-flush">
+          <li class="list-group-item">Rating: 4.5★</li>
+          <li class="list-group-item">Open Days: Mon - Fri</li>
+          <li class="list-group-item">Open times: 8am - 4pm</li>
+        </ul>
+      </div>
+      <div class="card card-item rounded-2" style="width: 18rem;">
+        <div class="img-container">
+          <img class="card-img-top" src="https://images.squarespace-cdn.com/content/v1/5c3cdd80c258b408c8f871c9/1637010787400-UYMKKSY0BO52T540YXK6/D1BDE42D-EAE9-4588-BC33-2C17E0CA8895_1_105_c.jpeg?format=2500w" alt="Card image cap">
+        </div>
+        <div class="card-body">
+          <h5 class="card-title">Hatfield</h5>
+          <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+        </div>
+        <ul class="list-group list-group-flush">
+          <li class="list-group-item">Rating: 4.5★</li>
+          <li class="list-group-item">Open Days: Mon - Fri</li>
+          <li class="list-group-item">Open times: 8am - 4pm</li>
+        </ul>
+      </div>
+      <div class="card card-item rounded-2" style="width: 18rem;">
+        <div class="img-container">
+          <img class="card-img-top" src="https://marylandwine.com/wp-content/uploads/2016/07/mdwine-winery-640x360.jpg" alt="Card image cap">
+        </div>
+        <div class="card-body">
+          <h5 class="card-title">Hatfield</h5>
+          <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+        </div>
+        <ul class="list-group list-group-flush">
+          <li class="list-group-item">Rating: 4.5★</li>
+          <li class="list-group-item">Open Days: Mon - Fri</li>
+          <li class="list-group-item">Open times: 8am - 4pm</li>
+        </ul>
+      </div>
+      <div class="card card-item rounded-2" style="width: 18rem;">
+        <div class="img-container">
+          <img class="card-img-top" src="https://cdn.shopify.com/s/files/1/0475/7743/2217/files/home-hill-winery-2021-FB_00ce056b-cc75-4f40-968c-50cbb09f402b.jpg?v=1623201223" alt="Card image cap">
+        </div>
+        <div class="card-body">
+          <h5 class="card-title">Hatfield</h5>
+          <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+        </div>
+        <ul class="list-group list-group-flush">
+          <li class="list-group-item">Rating: 4.5★</li>
+          <li class="list-group-item">Open Days: Mon - Fri</li>
+          <li class="list-group-item">Open times: 8am - 4pm</li>
+        </ul>
+      </div>
+      <div class="card card-item rounded-2" style="width: 18rem;">
+        <div class="img-container">
+          <img class="card-img-top" src="https://bplakecc.wpenginepowered.com/wp-content/uploads/2023/04/32431807_web1_230420-SUM-Sumac-closing-SUMMERLAND_1.jpg" alt="Card image cap">
+        </div>
+        <div class="card-body">
+          <h5 class="card-title">Hatfield</h5>
+          <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+        </div>
+        <ul class="list-group list-group-flush">
+          <li class="list-group-item">Rating: 4.5★</li>
+          <li class="list-group-item">Open Days: Mon - Fri</li>
+          <li class="list-group-item">Open times: 8am - 4pm</li>
+        </ul>
+      </div>
+      <div class="card card-item rounded-2" style="width: 18rem;">
+        <div class="img-container">
+          <img class="card-img-top" src="https://assets.simpleviewinc.com/simpleview/image/upload/c_limit,h_1200,q_75,w_1200/v1/clients/temecula/97aabe43407fcaa7e2c42ffe0db13936cf1f49f5aa290eed11c4055f5ffaaeac_06b79738-8707-40c7-a731-58ba4bcd799e.jpg" alt="Card image cap">
+        </div>
+        <div class="card-body">
+          <h5 class="card-title">Hatfield</h5>
+          <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+        </div>
+        <ul class="list-group list-group-flush">
+          <li class="list-group-item">Rating: 4.5★</li>
+          <li class="list-group-item">Open Days: Mon - Fri</li>
+          <li class="list-group-item">Open times: 8am - 4pm</li>
+        </ul>
+      </div>
+    </nav>
+
+    <!-- filter tab -->
+    <div class="modal fade modal-dialog-scrollable" id="exampleModal" tabindex="-1" aria-labelledby="exampleModalLabel" aria-hidden="true">
+      <div class="modal-dialog">
+        <div class="modal-content">
+          <div class="modal-header">
+            <h5 class="modal-title text-dark" id="exampleModalLabel">Filter wineries</h5>
+            <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
+          </div>
+          <div class="modal-body">
+            <h6 class="text-dark">Price ranges</h6>
+            <label for="customRange1" class="form-label text-dark">Min price for wines sold at winery: $2000</label>
+            <input type="range" class="form-range" id="customRange2">
+            <label for="customRange1" class="form-label text-dark">Max price for wines sold at winery: $5000</label>
+            <input type="range" class="form-range" id="customRange1">
+
+            <h6 class="text-dark">Locations</h6>
+            <div class="form-check form-check-inline">
+              <input class="form-check-input" type="checkbox" id="inlineCheckbox1" value="option1">
+              <label class="form-check-label text-dark" for="inlineCheckbox1">All</label>
+            </div>
+            <div class="form-check form-check-inline">
+              <input class="form-check-input" type="checkbox" id="inlineCheckbox2" value="option2">
+              <label class="form-check-label text-dark" for="inlineCheckbox2">Cape Town</label>
+            </div>
+            <div class="form-check form-check-inline">
+              <input class="form-check-input" type="checkbox" id="inlineCheckbox3" value="option3">
+              <label class="form-check-label text-dark" for="inlineCheckbox3">Port Elizabeth</label>
+            </div>
+            <div class="form-check form-check-inline">
+              <input class="form-check-input" type="checkbox" id="inlineCheckbox4" value="option4">
+              <label class="form-check-label text-dark" for="inlineCheckbox3">Durban</label>
+            </div>
+            <div class="form-check form-check-inline">
+              <input class="form-check-input" type="checkbox" id="inlineCheckbox5" value="option5">
+              <label class="form-check-label text-dark" for="inlineCheckbox3">Johannesburg</label>
+            </div>
+            <div class="form-check form-check-inline">
+              <input class="form-check-input" type="checkbox" id="inlineCheckbox6" value="option6">
+              <label class="form-check-label text-dark" for="inlineCheckbox3">Pretoria</label>
+            </div>
+            <div class="form-check form-check-inline">
+              <input class="form-check-input" type="checkbox" id="inlineCheckbox7" value="option7">
+              <label class="form-check-label text-dark" for="inlineCheckbox3">East London</label>
+            </div>
+            <div class="form-check form-check-inline">
+              <input class="form-check-input" type="checkbox" id="inlineCheckbox8" value="option8">
+              <label class="form-check-label text-dark" for="inlineCheckbox3">Pietermaritzburg</label>
+            </div>
+            <div class="form-check form-check-inline">
+              <input class="form-check-input" type="checkbox" id="inlineCheckbox9" value="option9">
+              <label class="form-check-label text-dark" for="inlineCheckbox3">Bloemfontein</label>
+            </div>
+            <label for="enteredLocation">Enter a location in South Africa</label>
+            <input type="text" class="form-control" id="enteredLocation" placeholder="Enter location here">
+          </div>
+          <div class="modal-footer">
+            <button type="button" class="btn btn-secondary" data-bs-dismiss="modal">Close</button>
+            <button type="button" class="btn" style="background-color: var(--app-theme-col);">Update filters</button>
+          </div>
+        </div>
+      </div>
+    </div>
+    <?php include "../Components/Footer.php";?>
+    <script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.11.7/dist/umd/popper.min.js" integrity="sha384-zYPOMqeu1DAVkHiLqWBUTcbYfZ8osu1Nd6Z89ify25QV9guujx43ITvfi12/QExE" crossorigin="anonymous"></script>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha3/dist/js/bootstrap.min.js" integrity="sha384-Y4oOpwW3duJdCWv5ly8SCFYWqFDsfob/3GkgExXKV4idmbt98QcxXYs9UoXAB7BZ" crossorigin="anonymous"></script>
+</body>
+</html>
