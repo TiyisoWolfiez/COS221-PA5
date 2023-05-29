70c3069d6ae255eecdd52e9f1728a0c65e14cc9f
Michael <u21546551@tuks.co.za>
2023-05-29 21:05:29 +0200

    javascript files creation

--- project/FrontEnd/Pages/wineries.php
+++ project/FrontEnd/Pages/wineries.php
@@ -145,7 +145,6 @@
     </nav>
 
     <!-- filter tab -->
-    <!-- filter tab -->
     <div class="modal fade modal-dialog-scrollable" id="exampleModal" tabindex="-1" aria-labelledby="exampleModalLabel" aria-hidden="true">
       <div class="modal-dialog">
         <div class="modal-content">
@@ -209,8 +208,10 @@
         </div>
       </div>
     </div>
+    <!-- filter tab -->
     <?php include "../Components/Footer.php";?>
     <script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.11.7/dist/umd/popper.min.js" integrity="sha384-zYPOMqeu1DAVkHiLqWBUTcbYfZ8osu1Nd6Z89ify25QV9guujx43ITvfi12/QExE" crossorigin="anonymous"></script>
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha3/dist/js/bootstrap.min.js" integrity="sha384-Y4oOpwW3duJdCWv5ly8SCFYWqFDsfob/3GkgExXKV4idmbt98QcxXYs9UoXAB7BZ" crossorigin="anonymous"></script>
+    <script src="../Client/wineries.js"></script>
 </body>
 </html>
